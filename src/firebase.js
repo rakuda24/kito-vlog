e5b257ac3d1f4cc2dd08f469b075ed7164d83526
rakuda24 <aichitead23302@gmail.com>
2024-11-03 22:23:57 +0900

    firstcommit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// firebase.js
+
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA3sN_rv6ERS2-rVBTKZRrpvtTFayMu80I",
+    authDomain: "kito-vlog.firebaseapp.com",
+    projectId: "kito-vlog",
+    storageBucket: "kito-vlog.firebasestorage.app",
+    messagingSenderId: "624959048355",
+    appId: "1:624959048355:web:804f3e21c2eef0c6618d16"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export { auth, provider};
